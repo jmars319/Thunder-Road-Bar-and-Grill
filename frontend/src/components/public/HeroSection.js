ca1efe92229c439aa29db311e3af30edb090cf72
Jason Marshall <jason@example.com>
2025-12-28 03:00:06 -0500

    perf(hero): wire static responsive hero assets

--- frontend/src/components/public/HeroSection.js
+++ frontend/src/components/public/HeroSection.js
@@ -37,11 +37,21 @@ const DEFAULT_HERO_COPY = {
   ]
 };
 const STATIC_HERO_META = {
-  webpSrcset: '/static/hero/hero-default-1x.webp 1x, /static/hero/hero-default-2x.webp 2x',
-  jpgSrcset: '/static/hero/hero-default-1x.jpg 1x, /static/hero/hero-default-2x.jpg 2x',
-  fallbackSrc: '/static/hero/hero-default-1x.jpg',
-  width: 2880,
-  height: 1920,
+  webpSrcset: [
+    '/static/hero/hero-default-640.webp 640w',
+    '/static/hero/hero-default-960.webp 960w',
+    '/static/hero/hero-default-1x.webp 1440w',
+    '/static/hero/hero-default-2x.webp 2880w'
+  ].join(', '),
+  jpgSrcset: [
+    '/static/hero/hero-default-640.jpg 640w',
+    '/static/hero/hero-default-960.jpg 960w',
+    '/static/hero/hero-default-1x.jpg 1440w',
+    '/static/hero/hero-default-2x.jpg 2880w'
+  ].join(', '),
+  fallbackSrc: '/static/hero/hero-default-1x.webp',
+  width: 1440,
+  height: 960,
   alt: 'Thunder Road Bar and Grill interior'
 };
 
